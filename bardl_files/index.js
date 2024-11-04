2e6ef043fdb042baf089e8978a170a567ea8774b
Gagik Movsisyan <gagikm@ucla.edu>
2024-11-03 19:29:13 -0800

    add interval

--- bardl_files/index.js
+++ bardl_files/index.js
@@ -1,9 +1,22 @@
 import { pipeline, env } from "https://cdn.jsdelivr.net/npm/@huggingface/transformers";
 
+const status = JSON.parse(localStorage.getItem("gameState")).gameStatus;
 env.allowLocalModels = false;
-const translator = await pipeline('translation', 'Xenova/m2m100_418M');
-const output = await translator('մածուն', {
-  src_lang: 'hy', // Armenian
-  tgt_lang: 'en', // English
-});
-console.log(output);
+setInterval(() => {
+	if (status === "WIN" || status === "FAIL") {
+		const translator = await pipeline('translation', 'Xenova/m2m100_418M');
+		const output = await translator('մածուն', {
+		  src_lang: 'hy', // Armenian
+		  tgt_lang: 'en', // English
+		});
+		console.log(output);
+
+		const modalElement = document.querySelectorAll("game-app")[0].shadowRoot.querySelector("game-theme-manager").querySelector("game-modal");
+		const newElement = document.createElement("div");
+		newElement.textContent = output;
+		modalElement.appendChild(newElement);
+
+	}
+
+}, 1000);
+
